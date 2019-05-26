12f0b053e4005f38448b6d52ac18c56b3c3f2f3f
maickol12 <miguel.angel.rodriguez.cornejo@gmail.com>
2019-05-26 16:28:25 -0500

    update2

--- quiniela/Controllers/UserAdministrationController.cs
+++ quiniela/Controllers/UserAdministrationController.cs
@@ -1,4 +1,6 @@
-﻿using quiniela.Models;
+﻿using PagedList.Mvc;
+using PagedList;
+using quiniela.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,17 +12,69 @@ namespace quiniela.Controllers
     public class UserAdministrationController : Controller
     {
         // GET: UserAdministration
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder,string searchBy,String search,int? page)
         {
-            
-            using(var db = new databaseModelContext())
+            if (!isLogin())
+            {
+                return RedirectToAction("index", "User");
+            }
+            searchBy                 = (searchBy == null) ? "" : searchBy;
+            ViewBag.sortVname        = (sortOrder == "vNameAsc") ? "vNameDesc" : "vNameAsc";
+            ViewBag.sortVlastName    = (sortOrder == "vLastNameAsc")? "vLastNameDesc": "vLastNameAsc";
+            ViewBag.SortvUsuario     = (sortOrder == "vUsuarioAsc")?"vUsuarioDesc":"vUsuarioAsc";
+
+            using (var db = new databaseModelContext())
             {
                 var users = from s in db.tblUsers
                             select s;
+
                 
-                return View(users.ToList());
+                switch (sortOrder)
+                {
+                    case "vNameAsc":
+                        users = users.OrderBy(s => s.vName);
+                    break;
+                    case "vnameDesc":
+                         users = users.OrderByDescending(s => s.vName);
+                    break;
+                    case "vLastNameAsc":
+                         users = users.OrderBy(s => s.vLastName);
+                    break;
+                    case "vLastNameDesc":
+                        users = users.OrderByDescending(s => s.vLastName);
+                    break;
+                    case "vUsuarioAsc":
+                        users = users.OrderBy(s => s.vUserName);
+                        break;
+                    case "vUsuarioDesc":
+                        users = users.OrderByDescending(s => s.vUserName);
+                        break;
+                    default:
+                        users = users.OrderBy(s => s.idUser);
+                    break;
+                }
+                if (searchBy.ToLower().Trim().Equals("name"))
+                {
+                    if (!search.Equals(""))
+                    {
+                        users = users.Where(s => s.vName.Contains(search.Trim()));
+                    }
+                    
+                } 
+                return View(users.ToPagedList(page ?? 1, 10));
             }
-            
+        }
+        private bool isLogin()
+        {
+            try
+            {
+                bool isLogin = (bool)Session["isLogin"];
+                return isLogin;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
         }
     }
 }
